412fc30932b34b6873f6f2b2e074d42f331164f1
cloudlamptech@gmail.com <pavankumarbakku@Pavans-MacBook-Air.local>
2025-03-22 22:47:40 +0530

    updated hostname to postgres

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "postgresql",
+  host: "postgres",
   // host: "13.201.82.137",
   database: "postgres",
   password: "post123",
