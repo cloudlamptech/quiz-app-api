bc19efe3a7c2f59942835a2837b70ee4cfe9f09d
cloudlamptech@gmail.com <pavankumarbakku@Pavans-MacBook-Air.local>
2025-03-25 18:01:49 +0530

    updated the postgresql conn

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "postgresql",
-  // host: "13.201.82.137",
-  database: "postgres",
-  password: "post123",
-  port: 5432,
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "postgres",
+  database: process.env.DB_NAME || "quiz_db",
+  password: process.env.DB_PASSWORD || "postgres",
+  port: process.env.DB_PORT || 5432,
 });
 
 // Test database connection
