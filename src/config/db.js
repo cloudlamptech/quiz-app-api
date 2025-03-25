73d0516966f039cd5fd5b08e0a719bfdada0b50e
cloudlamptech@gmail.com <pavankumarbakku@Pavans-MacBook-Air.local>
2025-03-25 23:01:22 +0530

    Added docker compose

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "postgres",
-  database: process.env.DB_NAME || "quiz_db",
+  database: process.env.DB_NAME || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
   port: process.env.DB_PORT || 5432,
 });
